6f8582aaab5cf6ec943359359191cc69fc0f31c5
fengj <fengj@FengJ-TPC>
2015-05-06 10:52:50 +0800

    first

---
+++ src/com/neusoft/mid/gittool/Boot.java
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ * @(#)Boot.java 2015年5月6日
+ *
+ * Copyright 2015 Neusoft Group Ltd. All rights reserved.
+ * Neusoft PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *******************************************************************************/
+package com.neusoft.mid.gittool;
+
+/**
+ * TODO 这里请补充该类型的简述说明
+ * @author <a href="mailto:feng.jian@neusoft.com">feng.jian</a>
+ * @version $Revision 1.0 $ 2015年5月6日 上午10:52:22
+ */
+public class Boot {
+
+}
